01393758964137486cdb82e7bc2acd08ee887685
unknown <dipak1920@gmail.com>
2023-09-24 19:44:55 +0545

    condition of checking correct input is moved to play round function

---
+++ rock.js
@@ -0,0 +1,48 @@
+function get_player_selection(){
+    human_choice = prompt("What is your move:") .toLowerCase()
+        return human_choice;
+}
+
+function get_computer_choice(){
+    let option = ["R","P","S"]
+    let randomIndex = Math.floor(Math.random() * option.length);
+    return option[randomIndex].toLowerCase();
+
+}
+k = get_computer_choice()
+l = get_player_selection()
+
+function play_game(){
+    if ((l !== "r" && l !== "p" && l !=="s")){
+        console.log("Invlid move by player")
+    }
+    else{
+        console.log("The game begins:")
+    if (k == l){
+        console.log("The match is draw")
+    }
+    else if (k == "r" && l == "s"){
+        console.log(`computer moved ${k} and won the game`)
+        console.log(`You entered ${l}`)
+        console.log("You lose!")
+    }
+    else if (k == "p" && l == "r"){
+        console.log(`computer moved ${k} and won the game`)
+        console.log(`You entered ${l}`)
+        console.log("You lose!")
+    }
+    else if (k == "s" && l == "p"){
+        console.log(`computer moved ${k} and won the game`)
+        console.log(`You entered ${l}`)
+        console.log("You lose!")
+    }
+    else{
+        console.log(`player entered ${l} and won the game`)
+        console.log(`Computer entered ${k}`)
+        console.log("Congrulation you won the game")
+    }
+    }
+    
+}
+play_game()
+
