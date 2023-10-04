3e1927104eea494a51922ec09969ce8372a28ab6
unknown <dipak1920@gmail.com>
2023-10-04 17:28:18 +0545

    removed battleground class

--- rock.js
+++ rock.js
@@ -1,5 +1,38 @@
 document.addEventListener('DOMContentLoaded',()=>{
     
+        let game = document.getElementById('start')
+        let change = document.querySelector('body') // Targetting the body so then when button is clicked then everything that exist gets removed and Rock Paper Scissor game get's displayed
+
+        let play = document.getElementById('playground')
+
+        game.addEventListener('click',() =>{
+            change.innerHTML='';
+            change.style.backgroundImage='none';
+            // change.style.backgroundColor='red';
+
+            let first = document.createElement('div')
+            first.classList='one';
+            first.style.border='3 px solid green';
+            first.style.backgroundColor='pink';
+            first.style.textAlign = 'center';
+            change.append(first)
+            let second = document.createElement('h1')
+            second.innerHTML='Welcome to our Rock Paper Scissor Game';
+            second.style.color = 'green';
+            second.style.fontStyle='italic';
+            second.style.fontWeight='bolder';
+            second.style.textAlign='center';
+            first.append(second);
+            /* the future content goes here */
+            document.body.append(play);   // Since everything has vanished after clicking the button this will make the playing area to appear
+            if(play.style.display==='none'|| play.style.display===''){
+                play.style.display = 'flex';        
+
+            }
+            else{
+                play.style.display='none';
+            }       
+        })     
 let computer_score = 0;
 let player_score = 0;
 let user_choice = "";
@@ -41,7 +74,7 @@ rock.addEventListener('click',()=>{
     user_choice = ("R").toLowerCase()
     // document.querySelector('h1').innerHTML = user_choice;
     play_game()
-    document.querySelector('h2').innerHTML =`Computer Score: ${k}`;
+    document.querySelector('h2').innerHTML =`Computer Choice: ${k}`;
     
 })
 paper = document.getElementById('paper');
@@ -49,25 +82,24 @@ paper.addEventListener('click',()=>{
     user_choice = ("P").toLowerCase()
     // document.querySelector('h1').innerHTML = user_choice;
     play_game()
-    document.querySelector('h2').innerHTML = `Computer Score: ${k}`;
+    document.querySelector('h2').innerHTML = `Computer Choice: ${k}`;
 })
 scissor = document.getElementById('scissor');
 scissor.addEventListener('click',()=>{
     user_choice = ("S").toLowerCase()
     // document.querySelector('h1').innerHTML = user_choice;
     play_game()
-    document.querySelector('h2').innerHTML = `Computer Score: ${k}`;
+    document.querySelector('h2').innerHTML = `Computer Choice: ${k}`;
 })
 function end_score(){
 if(computer_score ==5 || player_score == 5){
     if (computer_score==5){
-        document.body.innerHTML = `Computer won the game with the score of ${computer_score}`
+        second.innerHTML = `Computer won the game with the score of ${computer_score}`
     }
     else{
-        document.body.innerHTML = `Congrulation you won the game with the score of ${player_score}`;
+        second.innerHTML = `Congrulation you won the game with the score of ${player_score}`;
     }
 }
 }
-
 
 })
